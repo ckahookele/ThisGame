c0ca489d00a3dac118c612dd3d057efde7fc273a
ckahookele <calebkahookele@gmail.com>
2015-03-23 18:02:20 -0700

    hi

--- ThisGame/models.py
+++ ThisGame/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class Teacher(models.Model):
+    name = models.CharField(max_length=255)
+
+class Class(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=255)
+
+class Subject(models.Model):
+    name = models.CharField(max_length=255)
+
 class Period(models.Model):
     block = models.CharField(max_length=255)
     room = models.CharField(max_length=255)
